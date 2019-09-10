65862e0b830b62b18de233d339a624d7dec17186
Gemma Mason <gemmaellen@gmail.com>
2019-09-10 16:33:48 +1200

    Align with paper
    A set of changes designed to follow the paper of Lee et al (1990) more closely. I define a new set of basis vectors w_i which are transformed back into the original data space, and then attempt to use these rather than the z_i vectors to de-noise the data.

--- whiten_noise.m
+++ whiten_noise.m
@@ -1,9 +1,12 @@
-function data_whitened = whiten_noise(data, N)
+function [F, data_whitened] = whiten_noise(data, N)
 % Uses a noise estimate N to whiten the noise in data.
 % Input: data  -- A 2D array of data. The first dimension is of length
 %                 m1*m2 and contains spatial data. The second dimension
 %                 contains spectral data of length n.
 %        N     -- A matrix of noise estimates in each data point.
+% Output: data_whitened -- A 2D array of data transformed so as to whiten
+%                          the noise.
+%         F -- transformation matrix
 
 % Matrix of noise estimates should be the same size as our data.
 assert(all(size(data) == size(N)))
@@ -14,6 +17,6 @@ assert(all(size(data) == size(N)))
 Sn = sparse(Sn); % so MATLAB will know it's diagonal
 
 % noise whitening matrix
-F = (Vn / Sn) * Vn' ; % use right divide to compute inverse
+F = Vn / Sn; % use right divide to compute inverse
 
 data_whitened = data*F;
